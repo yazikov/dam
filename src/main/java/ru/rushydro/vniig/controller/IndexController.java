251f86fd25eaaa17de6d83f980d42e89e85a030f
nikolay <yazikov2007@mail.ru>
2015-09-06 18:16:28 +0300

    Рабочая версия

--- src/main/java/ru/rushydro/vniig/controller/IndexController.java
+++ src/main/java/ru/rushydro/vniig/controller/IndexController.java
@@ -14,4 +14,9 @@ public class IndexController {
     public String indexPage() {
         return "index";
     }
+
+    @RequestMapping(path = "/test")
+    public String testPage() {
+        return "test";
+    }
 }
